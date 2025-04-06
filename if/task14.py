65e1307026e800400d94b3771e20bd908866b943
kenjahacker <kenjaev.hasan001@gmail.com>
2025-04-06 10:08:20 +0500

    darkstar

--- if/task14.py
+++ if/task14.py
@@ -1,6 +1,6 @@
-a = int(input("a = "))
-b = int(input("b = "))
-c = int(input("c = "))
+a = int(input("Berilgan a: "))
+b = int(input("Berilgan b: "))
+c = int(input("Berilgan c: "))
 
 if a > 0 and b > 0 and c > 0 and (a + b + c == 180):
     print("true")
