b90570939a44fc4a0e66eb2910ae0827c501cf7d
darkstat0 <kenjaev.hasan001@gmail.com>
2025-04-11 03:31:24 +0500

    darkstar

---
+++ if/task25.py
@@ -0,0 +1,16 @@
+a = int(input("Berilgan a: "))
+b = int(input("Berilgan b: "))
+c = int(input("Berilgan c: "))
+d = int(input("Berilgan d: "))
+
+if a > 0 and b > 0 and c > 0 and d > 0:
+	print("Natija: true")
+elif a < 0:
+	print(a)
+elif b < 0:
+	print(b)
+elif c < 0:
+	print(c)
+elif d < 0:
+	print(d)	
+
