45281f63d6c646c3119008a40c5db602cf30e7fa
darkstat0 <kenjaev.hasan001@gmail.com>
2025-04-10 23:12:28 +0500

    darkstar

---
+++ if/task18.py
@@ -0,0 +1,9 @@
+a = int(input("a sonini kiriting: "))
+b = int(input("b sonini kiriting: "))
+
+if a < b:
+    print(a)
+elif b < a:
+    print(b)
+else:
+    print("teng")
---
+++ if/task19.py
@@ -0,0 +1,6 @@
+a = int(input("a sonini kiriting: "))
+b = int(input("b sonini kiriting: "))
+c = int(input("c sonini kiriting: "))
+
+average = (a + b + c) // 3
+print(average)
