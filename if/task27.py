1898dadfea1d77d001e6d5d6b3bc879306087fae
darkstat0 <kenjaev.hasan001@gmail.com>
2025-04-11 22:08:49 +0500

    darkstar

--- if/task27.py
+++ if/task27.py
@@ -2,9 +2,39 @@ a = int(input("Berilgan a: "))
 b = int(input("Berilgan b: "))
 c = int(input("Berilgan c: "))
 
-if (a % 2 == 0 and b % 2 == 0 and c % 2 == 0):
-	print("Natija:", 1)
-elif (a % 2 == 1 and b % 2 == 1 and c % 2 == 1):
-	print("Natija:", 2)
+if a % 2 == 0:
+	if b % 2 == 0:
+		if c % 2 == 0:
+			print("Natija: 1")
+	elif b % 2 == 1:
+		pass
+	elif c % 2 == 1:
+		print("Natija: ")
+	elif c % 2 == 0:
+		print("Natija: ")
+elif a % 2 == 1:
+	if b % 2 == 0:
+		if c % 2 == 0:
+			print("Natija: 2")
+	elif b % 2 == 1:
+		print("Natija: ")
+	elif c % 2 == 1:
+		print("Natija: ")
+	elif c % 2 == 0:
+		print("Natija: ")
+elif b % 2 == 0:
+	if c % 2 == 0:
+		print("Natija: 3")
+	elif c % 2 == 1:
+		pass
+elif b % 2 == 1:
+	if c % 2 == 0:
+		print("Natija: ")
+	elif c % 2 == 1:
+		print("Natija: ")
+elif c % 2 == 0:
+	print("Natija: ")
+elif c % 2 == 1:
+	print("Natija: ")
 else:
-	print("Natija:", 0)
+	print("Natija: 0")
