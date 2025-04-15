9d10612eac6d7f3b5f2b02854b1cd4d8092a4e6b
darkstar <kenjaev.hasan001@gmail.com>
2025-04-16 03:47:16 +0500

    darkstar

--- if/task27.py
+++ if/task27.py
@@ -2,17 +2,21 @@ a = int(input("Berilgan a: "))
 b = int(input("Berilgan b: "))
 c = int(input("Berilgan c: "))
 
-if a and b % 2 == 0:
-	print("Natija: 1")
-elif b and c % 2 == 0:
-	print("Natija: 1")
-elif a and c % 2 == 0:
-	print("Natija: 1")
-elif a and b % 2 == 1:	
-	print("Natija: 2")
-elif b and c % 2 == 1:
-	print("Natija: 2")
-elif a and c % 2 == 1:
-	print("Natija: 2")
-else:
-	print("Natija: 0")
+evens = sum(x % 2 == 0 for x in (a, b, c))
+print(f"Natija: {1}" if evens == 2 else f"Natija: {2}" if evens == 1 else 3)
+
+
+# if a and b % 2 == 0:
+# 	print("Natija: 1")
+# elif b and c % 2 == 0:
+# 	print("Natija: 1")
+# elif a and c % 2 == 0:
+# 	print("Natija: 1")
+# elif a and b % 2 == 1:	
+# 	print("Natija: 2")
+# elif b and c % 2 == 1:
+# 	print("Natija: 2")
+# elif a and c % 2 == 1:
+# 	print("Natija: 2")
+# else:
+# 	print("Natija: 0")
