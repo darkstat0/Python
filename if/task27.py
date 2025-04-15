ad50c9bb65a1c2889dae9beef529b4310f5c5c1d
darkstar <kenjaev.hasan001@gmail.com>
2025-04-16 01:10:27 +0500

    darkstar

--- if/task27.py
+++ if/task27.py
@@ -2,39 +2,17 @@ a = int(input("Berilgan a: "))
 b = int(input("Berilgan b: "))
 c = int(input("Berilgan c: "))
 
-if a % 2 == 0:
-	if b % 2 == 0:
-		if c % 2 == 0:
-			print("Natija: 1")
-	elif b % 2 == 1:
-		pass
-	elif c % 2 == 1:
-		print("Natija: ")
-	elif c % 2 == 0:
-		print("Natija: ")
-elif a % 2 == 1:
-	if b % 2 == 0:
-		if c % 2 == 0:
-			print("Natija: 2")
-	elif b % 2 == 1:
-		print("Natija: ")
-	elif c % 2 == 1:
-		print("Natija: ")
-	elif c % 2 == 0:
-		print("Natija: ")
-elif b % 2 == 0:
-	if c % 2 == 0:
-		print("Natija: 3")
-	elif c % 2 == 1:
-		pass
-elif b % 2 == 1:
-	if c % 2 == 0:
-		print("Natija: ")
-	elif c % 2 == 1:
-		print("Natija: ")
-elif c % 2 == 0:
-	print("Natija: ")
-elif c % 2 == 1:
-	print("Natija: ")
+if a and b % 2 == 0:
+	print("Natija: 1")
+elif b and c % 2 == 0:
+	print("Natija: 1")
+elif a and c % 2 == 0:
+	print("Natija: 1")
+elif a and b % 2 == 1:	
+	print("Natija: 2")
+elif b and c % 2 == 1:
+	print("Natija: 2")
+elif a and c % 2 == 1:
+	print("Natija: 2")
 else:
 	print("Natija: 0")
