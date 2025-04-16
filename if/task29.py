a67fa7710624d7865fc34bc48e92b04e57d37d49
darkstar <kenjaev.hasan001@gmail.com>
2025-04-16 14:33:40 +0500

    darkstat

--- if/task29.py
+++ if/task29.py
@@ -3,4 +3,4 @@ b = int(input("Berilgan b: "))
 c = int(input("Berilgan c: "))
 
 zero = sum(x == 0 for x in (a, b, c))
-print(f"Natija: 0" if zero == 1 else f"{a}{b}{c}")
+print(f"Natija: 0" if zero == 1 else "Natija:", (a*100+b*10+c))
