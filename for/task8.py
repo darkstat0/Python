99bd4d79d90f34bdc32cd2a81af41528a9d7a63a
Darkstar Tracer <darkstat@ti-n4.skd.21-school.uz>
2025-06-15 18:56:01 +0500

    darkstat

--- for/task8.py
+++ for/task8.py
@@ -1,5 +1,10 @@
 a = int(input("Berilgan a: "))
 b = int(input("Berilgan b: "))
+son = []
 
 for i in range(a, b):
-	if i%3 ==
+	if i%3 == 0:
+		son.append(i)
+
+print("yig'indi =", sum(son))
+print("soni =", len(son))
