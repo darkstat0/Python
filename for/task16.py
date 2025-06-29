eeda2177eafa60ef9813615942f7937268193b10
darkstat0 <kenjaev.hasan001@gmail.com>
2025-06-29 10:16:45 +0500

    darkstar

--- for/task16.py
+++ for/task16.py
@@ -1,13 +1,25 @@
-n = (input("Berilgan n: "))
-l = list(n)
-a = 0
+# n = (input("Berilgan n: "))
+# l = list(n)
+# a = 0
 
-for i in range(len(l)):
-	son = int(l[i])
-	a += son**len(l)
-	print(a)
+# for i in range(len(l)):
+# 	son = int(l[i])
+# 	a += son**len(l)
+# 	print(a)
 
-if int(n) == a:
-	print("Natija:", True)
-else:
-	print("Natija:", False)
+# if int(n) == a:
+# 	print("Natija:", True)
+# else:
+# 	print("Natija:", False)
+
+# =============================================================================
+
+# n = input("Berilgan n: ")
+# a = sum(int(digit) ** len(n) for digit in n)
+
+# print("Natija:", int(n) == a)
+
+# =============================================================================
+
+n = input("Berilgan n: ")
+print("Natija:", int(n) == sum(int(digit) ** len(n) for digit in n))
