da19b22fc2362a0c9da6b4ced9e96b2faef5dc54
darkstat0 <kenjaev.hasan001@gmail.com>
2025-06-29 09:51:59 +0500

    darkstar

--- for/task16.py
+++ for/task16.py
@@ -1,13 +1,13 @@
-n = int(input("Berilgan n: "))
-l = str(n)
-l = list(l)
+n = (input("Berilgan n: "))
+l = list(n)
 a = 0
 
 for i in range(len(l)):
 	son = int(l[i])
 	a += son**len(l)
+	print(a)
 
-if n == a:
+if int(n) == a:
 	print("Natija:", True)
 else:
 	print("Natija:", False)
